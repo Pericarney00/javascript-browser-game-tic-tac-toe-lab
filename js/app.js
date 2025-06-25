272f5529dbf1401656f059c614589940dd284a19
Peri Carney <pericarney@gmail.com>
2025-06-25 18:53:25 -0500

    established winningCombos

--- js/app.js
+++ js/app.js
@@ -28,7 +28,16 @@
 const squareEls = document.querySelectorAll(".sqr")
 const messageEl = document.querySelector("#message")
 console.log(messageEl)
-
+const winningCombos = [
+    [0,1,2], 
+    [3,4,5], 
+    [6,7,8], 
+    [0,4,8],
+    [2,4,6],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8]   
+    ]
 /*---------------------------- Variables (state) ----------------------------*/
  let board = ["","","","","","","","",""]
  let turn = "X"
